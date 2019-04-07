7570f69c20fde7ca19eb5ebbde6ec0b22da39451
MihutBalasanu <Mihut.Balasanu@gmail.com>
2019-04-07 12:45:24 +0300

    Test directory commit

--- booking-rest/booking-rest-loader/test/com/sda/booking/core/service/PaymentImplTest.java
+++ booking-rest/booking-rest-loader/test/com/sda/booking/core/service/PaymentImplTest.java
@@ -1,8 +1,6 @@
 package com.sda.booking.core.service;
 
-import com.sda.booking.core.entity.Booking;
-import com.sda.booking.core.entity.Client;
-import com.sda.booking.core.entity.Payment;
+import com.sda.booking.core.entity.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +21,10 @@ public class PaymentImplTest {
 
     @Autowired
     private PaymentService paymentService;
+    private ClientService clientService;
+    private BookingService bookingService;
+    private PropertyService propertyService;
+    private HostService hostService;
 
     @Test
     @Rollback(false)
@@ -35,14 +37,17 @@ public class PaymentImplTest {
         cal.set(2019, 03, 12);
         Date date = cal.getTime();
         payment.setPaymentDate(date);
-        paymentService.createPayment(payment);
+
         Assert.assertEquals(new BigDecimal(10), payment.getAmount());
         Booking booking = new Booking();
+
         Client client = new Client();
         client.setName("Ion Popescu");
         client.seteMail("ionpopescu@gmail.com");
         client.setPhone("0754132145");
+        clientService.createClient(client);
         Long clientId = client.getId();
+
         booking.setId(clientId);
         cal.set(2019,04,15);
         date = cal.getTime();
@@ -55,9 +60,34 @@ public class PaymentImplTest {
         booking.setCheckOut(date);
         booking.setNumberOfPersons(2);
         booking.setNumberOfRooms(1);
+
+
+        Property property = new Property();
+        property.setName("Hotel Alpin");
+        property.seteMail("hotelalpin.@yahoo.com");
+        property.setPhone("0765432123");
+        property.setAddress("Poiana Brasov");
+
+        Host host = new Host();
+        host.setName("Dan Negru");
+        host.seteMail("dannegru@yahoo.com");
+        hostService.createHost(host);
+
+        Long hostId = host.getId();
+        propertyService.createProperty(property);
+        property.setId(hostId);
+        Long propertyId = property.getId();
+        booking.setId(propertyId);
+        bookingService.createBooking(booking);
         Long bookingId = booking.getId();
+
         payment.setId(bookingId);
-        
+        paymentService.createPayment(payment);
+
+        Assert.assertEquals(new BigDecimal(10), payment.getAmount());
+
+
+
 
 
 
