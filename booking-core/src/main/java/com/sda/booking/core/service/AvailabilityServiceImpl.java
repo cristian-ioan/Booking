de7c08c5ec2f87ab42268b2339605638495e85f8
Cristian Lucian IOAN <cristian-ioan@users.noreply.github.com>
2019-04-14 10:16:12 +0300

    findAllPropertiesAvailableDuringACertainDateInterval commit

--- booking-core/src/main/java/com/sda/booking/core/service/AvailabilityServiceImpl.java
+++ booking-core/src/main/java/com/sda/booking/core/service/AvailabilityServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service("availabilityService")
@@ -42,5 +43,10 @@ public class AvailabilityServiceImpl implements AvailabilityService {
     @Transactional
     public void deleteAvailability(Availability availability) {
         availabilityRepository.delete(availability);
+    }
+
+    @Override
+    public List<Availability> findAllPropertiesAvailableDuringACertainDateInterval(LocalDate fromDate, LocalDate toDate) {
+        return availabilityRepository.findAvailabilitiesByFromDateGreaterThanEqualAndToDateLessThanEqual(fromDate, toDate);
     }
 }
